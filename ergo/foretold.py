4e8807f5311083e1b00964fc27ddfd3bf02a0b84
Will Baizer <will@pullrequest.com>
2020-04-29 12:50:02 -0500

    revert back to original start

--- ergo/foretold.py
+++ ergo/foretold.py
@@ -8,24 +8,32 @@ from ergo.ppl import uniform
 class Foretold:
     """Interface to Foretold"""
 
-    def __init__(self, token=None):
-        """token (string): Specify an authorization token (supports Bot tokens from Foretold)"""
-        self.token = token
-        self.api_url = "https://prediction-backend.herokuapp.com/graphql"
+    def __init__(self):
+        pass
 
     def get_question(self, id):
-        question = ForetoldQuestion(id, self)
+        question = ForetoldQuestion(id)
         question.refresh_question()
         return question
 
-    def get_measurable(self, id):
-        headers = {}
-        if self.token is not None:
-            headers["Authorization"] = f"Bearer {self.token}"
+
+class ForetoldQuestion:
+    """"Information about foretold question, including aggregated distribution"""
+
+    def __init__(self, id):
+        """
+            id: measurableId, the second id in the URL for a foretold question
+        """
+        self.id = id
+        self.floatCdf = None
+        self.channelId = None
+
+    def refresh_question(self):
+        # previousAggregate is the most recent aggregated distribution
         response = requests.post(
-            self.api_url,
+            "https://prediction-backend.herokuapp.com/graphql",
             json={
-                "variables": {"measurableId": id},
+                "variables": {"measurableId": self.id},
                 "query": """query ($measurableId: String!) {
                                 measurable(id:$measurableId) {
                                     id
@@ -41,27 +49,10 @@ class Foretold:
                                 }
                             }""",
             },
-            headers=headers,
         )
-        return response.json()["data"]["measurable"]
-
-
-class ForetoldQuestion:
-    """"Information about foretold question, including aggregated distribution"""
-
-    def __init__(self, id, foretold):
-        """
-            id: measurableId, the second id in the URL for a foretold question
-        """
-        self.id = id
-        self.foretold = foretold
-        self.floatCdf = None
-        self.channelId = None
-
-    def refresh_question(self):
-        # previousAggregate is the most recent aggregated distribution
+        response_json = response.json()
         try:
-            measurable = self.foretold.get_measurable(self.id)
+            measurable = response_json["data"]["measurable"]
             self.channelId = measurable["channelId"]
             self.floatCdf = measurable["previousAggregate"]["value"]["floatCdf"]
         except KeyError:
@@ -71,14 +62,28 @@ class ForetoldQuestion:
     def url(self):
         return f"https://www.foretold.io/c/{self.channelId}/m/{self.id}"
 
-    def quantile(self, q):
-        """Quantile of distribution"""
-        return np.interp(q, self.floatCdf["ys"], self.floatCdf["xs"])
-
     def sample_community(self):
-        """Sample from CDF"""
+        """Sample from CDF
+
+        Assumes that xs are the x coordinates of the left edge of bins,
+        ys are the y coordinates of the left edge. First sample between 0 and 1,
+        find the corresponding bin, then linearly interpolate within the bin.
+
+        """
+        xs = torch.tensor(self.floatCdf["xs"])
+        ys = torch.tensor(self.floatCdf["ys"])
         y = uniform()
-        return torch.tensor(self.quantile(y))
+        # Finds the first index in ys where the value is greater than y.
+        # y then falls between ys[i-1] and ys[i]
+        i = np.argmax(ys > y)
+        if i == 0:
+            return xs[0]
+        x0 = xs[i - 1]
+        x1 = xs[i]
+        y0 = ys[i - 1]
+        y1 = ys[i]
+        w = (y - y0) / (y1 - y0)
+        return x1 * w + x0 * (1 - w)
 
     def plotCdf(self):
         seaborn.lineplot(self.floatCdf["xs"], self.floatCdf["ys"])
